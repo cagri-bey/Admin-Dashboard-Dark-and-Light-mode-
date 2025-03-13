31306fadb2097ffed1e6fb66e053727dc12159d3
Çağrı Aydın <cagriaydinoffical@gmail.com>
2025-03-13 04:12:58 +0300

    Add files via upload

---
+++ order.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '001',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'CSS Full Tutorial',
+        productNumber: '002',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'React.js Tutorial',
+        productNumber: '003',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
